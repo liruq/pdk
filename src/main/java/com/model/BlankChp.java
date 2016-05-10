cacd63c0c06d5a5a4bd5c85ff9f5d14431294e99
li <8557690a>
2016-05-10 20:27:54 +0800

    exam

---
+++ src/main/java/com/model/BlankChp.java
@@ -0,0 +1,28 @@
+package com.model;
+
+public class BlankChp {
+	private int id;//�½ں�
+	private float chp_r;//�½ڱ���
+	private String chp;//�½�����
+	
+	public void setId(int id){
+		this.id=id;
+	}
+	public void setChp_r(float chp_r){
+		this.chp_r=chp_r;
+	}
+	public void setChp(String chp){
+		this.chp=chp;
+	}
+	
+	public String getChp(){
+		return chp;
+	}
+	public float getChp_r(){
+		return chp_r;
+	}
+	public int getId(){
+		return id;
+	}
+
+}
--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,25 +1,25 @@
-import com.erp.model.User;
-import com.erp.service.UserService;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import javax.annotation.Resource;
-
-/**
- * Created by li on 2016/5/5.
- */
-
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations={"classpath*:spring.xml"})
-public class MyTest {
-    @Resource
-    UserService userService;
-
-    @Test
-    public void test(){
-        User user=userService.get("admin");
-        System.out.println(user.getPassword());
-    }
-}
+//import com.model.User;
+//import com.erp.service.UserService;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.test.context.ContextConfiguration;
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+//
+//import javax.annotation.Resource;
+//
+///**
+// * Created by li on 2016/5/5.
+// */
+//
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(locations={"classpath*:spring.xml"})
+//public class MyTest {
+//    @Resource
+//    UserService userService;
+//
+//    @Test
+//    public void test(){
+//        User user=userService.get("admin");
+//        System.out.println(user.getPassword());
+//    }
+//}
